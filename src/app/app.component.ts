5078aa6bdf108e711d9f94a3ab1d54aa6afdee8d
Tekla <davidannatekla@gmail.com>
2021-03-29 12:54:00 +0200

    add header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'simpleBlog';
+
+  post = [
+    {
+      id: 1,
+      title: "lorem",
+      body: "lorem iőpsum lorem ipsum"
+    },
+    {
+      id: 2,
+      title: "lorem",
+      body: "lorem iőpsum lorem ipsum"
+    },
+    {
+      id: 3,
+      title: "lorem",
+      body: "lorem iőpsum lorem ipsum"
+    },
+    {
+      id: 4,
+      title: "lorem",
+      body: "lorem iőpsum lorem ipsum"
+    },
+  ]
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]

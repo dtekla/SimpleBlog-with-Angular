abc8284e65ab137a02e1b5270fb32543cdd671a4
Tekla <davidannatekla@gmail.com>
2021-04-17 09:54:24 +0200

    interface service done

--- src/app/posts/comments/comment.service.ts
+++ src/app/posts/comments/comment.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpErrorResponse, HttpParams} from "@angular/common/http";
+import {Observable, throwError} from "rxjs";
+import {Comment} from "./comment.interface";
+import {catchError, retry} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommentService {
 
-  constructor() { }
+  static readonly API_URL = "http://localhost:3000/comments"
+
+  constructor(private http: HttpClient) { }
+
+  getComments(postId:number): Observable<Comment[]> {
+    // @ts-ignore
+    return this.http.get<Comment[]>(CommentService.API_URL,{
+      params: new HttpParams().set('_postId', postId.toString())
+    }).pipe(
+      retry(1),
+      catchError(this.handleError)
+    );
+  }
+
+  handleError(error: HttpErrorResponse): Observable<string> {
+    let errorMessage = '';
+
+    if(error.error instanceof ProgressEvent) {
+      errorMessage = `Error message: ${error.message}`;
+    } else {
+      errorMessage = `Error message: ${error.message}`;
+    }
+
+    return throwError(errorMessage);
+  }
 }
+

329f933c3f7b6dd01cf45f0b96d86b7db5cc20c3
Tekla <davidannatekla@gmail.com>
2021-04-19 18:45:24 +0200

    edit contact form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {ContactModule} from "./contact/contact.module";
 import {RouterModule} from "@angular/router";
 
 
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -31,7 +32,7 @@ import {RouterModule} from "@angular/router";
     MatButtonModule,
     PostsModule,
     ContactModule,
-    RouterModule
+    RouterModule,
   ],
   providers: [],
   exports: [

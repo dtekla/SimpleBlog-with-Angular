a89c2a963880eeaa3df4ffce5e121263b004ef69
Tekla <davidannatekla@gmail.com>
2021-05-05 19:20:02 +0200

    edit comment form validation

--- src/app/comments/comment.service.ts
+++ src/app/comments/comment.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpErrorResponse, HttpParams} from "@angular/common/http";
-import {Observable, throwError} from "rxjs";
+import {HttpClient, HttpParams} from "@angular/common/http";
+import {Observable} from "rxjs";
 import {Comment} from "./comment.interface";
-import {catchError, retry} from "rxjs/operators";
+
 import {NewComment} from "./newComment.interface";
 
 @Injectable({
@@ -10,39 +10,21 @@ import {NewComment} from "./newComment.interface";
 })
 export class CommentService {
 
-  static readonly API_URL = "http://localhost:3000/comments?_sort=id&_order=desc"
+  static readonly API_URL = "http://localhost:3000/comments"
 
-  constructor(private http: HttpClient) { }
-
-  getComments(postId:number): Observable<Comment[]> {
-    // @ts-ignore
-    return this.http.get<Comment[]>(CommentService.API_URL,{
-      params: new HttpParams().set('postId', postId.toString())
-    }).pipe(
-      retry(1),
-      catchError(this.handleError)
-    );
+  constructor(private http: HttpClient) {
   }
 
-  handleError(error: HttpErrorResponse): Observable<string> {
-    let errorMessage = '';
+  getComments(postId: number): Observable<Comment[]> {
 
-    if(error.error instanceof ProgressEvent) {
-      errorMessage = `Error message: ${error.message}`;
-    } else {
-      errorMessage = `Error message: ${error.message}`;
-    }
-
-    return throwError(errorMessage);
+    return this.http.get<Comment[]>(CommentService.API_URL, {
+      params: new HttpParams().set('_sort', 'id').append('_order', 'desc').append('postId', postId.toString())
+    })
   }
-
-  static readonly API_URL_COMMENT = `http://localhost:3000/comments/?postId=`
+  static readonly API_URL_COMMENT = `http://localhost:3000/comments`
 
   addComment(comment: NewComment, id: number): Observable<string | NewComment> {
-    return this.http.post<NewComment>(CommentService.API_URL_COMMENT +`/${id}`, comment).pipe(
-      retry(1),
-      catchError(this.handleError)
-    );
+    return this.http.post<NewComment>(CommentService.API_URL_COMMENT + `?postId=${id}`, comment);
   }
 
 }
